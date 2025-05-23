5d9d3f6dea739411d315bf4260ea7332d723b358
ShumTin <shum_tin0216@outlook.com>
2025-05-23 17:44:37 +0800

    搭建工程，完成Avatar，Icon，Pager组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>App组件</h1>
+    <Pager :total="800" :current="current" @pageChange="handlePageChange" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Pager from "./components/Pager.vue";
 export default {
-  name: "App",
+  data() {
+    return {
+      current: 5,
+    };
+  },
   components: {
-    HelloWorld,
+    Pager,
+  },
+  methods: {
+    handlePageChange(newPage) {
+      this.current = newPage;
+    },
   },
 };
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
